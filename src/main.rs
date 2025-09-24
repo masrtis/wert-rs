53b02a772224e91d7c5908f63753f565717fc401
Michael Sharpe <mike@sharpe.email>
2025-09-24 18:11:00 -0400

    Section 5 implementation

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,20 @@ use log::info;
 use ray::Ray;
 use vec3::{Point3, Vec3};
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = center - *r.origin();
+    let a = r.dir().length_squared();
+    let b = -2.0 * oc.dot(*r.dir());
+    let c = oc.length_squared().mul_add(1.0, -radius.powi(2));
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.dir().unit_vector();
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
